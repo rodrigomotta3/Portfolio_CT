bf006a7a2d683b54b6d682a6eeefbc2d4ac25971
rodrigomotta3 <rodrigo.motta@escola.pr.gov.br>
2025-11-19 14:38:04 -0300

    portfolio_19-11-2

--- scrip.js
+++ scrip.js
@@ -4,3 +4,9 @@ let texto = document.querySelector("#nome");
 let nome = prompt("Qual é o seu nome?");
 
 texto.textContent = nome;
+
+if (nome == null){
+    texto.textContent = 'Seja bem-vindo';
+}else{
+    texto.textContent = nome;
+}
